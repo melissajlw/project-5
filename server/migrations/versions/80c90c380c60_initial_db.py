d982eca020d23bf2d774a52e8c5ed4484ecdc33e
Melissa Wang <melissa.wang21@gmail.com>
2024-12-02 16:38:10 -0500

    initial commit

---
+++ server/migrations/versions/80c90c380c60_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: 80c90c380c60
+Revises: 
+Create Date: 2024-12-02 16:37:59.768362
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '80c90c380c60'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
